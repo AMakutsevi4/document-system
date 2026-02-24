6277a53bf1aec2959b5589ec26ccc0d90e6e594b
AMakutsevi4 <amakutsevich@bk.ru>
2026-02-24 23:33:00 +1000

    [Stage-1] add document-generator module

---
+++ document-service/src/main/resources/db/changelog/v-1.0/02-2026/2026-02-18--create-approval-registry-table.sql
@@ -0,0 +1,8 @@
+--liquibase formatted sql
+--changeset Alexandr Makutsevich:2026-02-18--create-approval-registry-table.sql
+
+CREATE TABLE approval_registry (
+    id BIGSERIAL PRIMARY KEY,
+    document_id BIGINT UNIQUE REFERENCES documents(id),
+    approved_at TIMESTAMP NOT NULL
+);
